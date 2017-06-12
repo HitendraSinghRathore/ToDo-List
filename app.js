e204f69cd4a9af04af09e27f63ee60bbdc15845c
Hitendra <hitendersingh2@gmail.com>
2017-06-12 19:29:18 +0530

    corrected the error

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function appController() {
     var todo = this;
     todo.tasks = [];
     todo.addTask = addTask;
+    var position = 0;
+    todo.editMode = false;
 
     function addTask() {
         todo.tasks.push(todo.task);
@@ -11,7 +13,17 @@ function appController() {
         todo.task = "";
 
     }
+    todo.update = function() {
+        todo.tasks[position] = todo.task;
+        todo.task = "";
+    }
     todo.deleteTask = function(index) {
         todo.tasks.splice(index, 1);
+    }
+    todo.editTask = function(index) {
+        position = index;
+        todo.task = todo.tasks[index];
+        todo.editMode = true;
+
     }
 }
