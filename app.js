dc303c7cbc120e81b504dd7db6eee932c684889c
Hitendra <hitendersingh2@gmail.com>
2017-06-16 05:49:07 +0530

    created reposotory

--- app.js
+++ app.js
@@ -4,25 +4,54 @@ function appController() {
     var todo = this;
     todo.tasks = [];
     todo.addTask = addTask;
+
     var position = 0;
     todo.editMode = false;
 
     function addTask() {
-        todo.tasks.push(todo.task);
-
+        var listItem = {};
+        listItem.title = todo.task;
+        listItem.status = false;
+        todo.tasks.push(listItem);
         todo.task = "";
+
 
     }
     todo.update = function() {
-        todo.tasks[position] = todo.task;
+        var temp = todo.task;
         todo.task = "";
+        todo.editMode = false;
+        todo.tasks[position].title = temp;
+
+
+
+
     }
     todo.deleteTask = function(index) {
         todo.tasks.splice(index, 1);
     }
+    todo.upTask = function(index) {
+        if (index > 0) {
+            var sw = {};
+            sw = todo.tasks[index];
+            todo.tasks[index] = todo.tasks[index - 1];
+            todo.tasks[index - 1] = sw;
+        }
+    }
+    todo.downTask = function(index) {
+        if (index < todo.tasks.length - 1) {
+            var sw = {};
+            sw = todo.tasks[index];
+            todo.tasks[index] = todo.tasks[index + 1];
+            todo.tasks[index + 1] = sw;
+        }
+    }
+    todo.doneTask = function(index) {
+        todo.tasks[index].status = !todo.tasks[index].status
+    }
     todo.editTask = function(index) {
         position = index;
-        todo.task = todo.tasks[index];
+        todo.task = todo.tasks[index].title;
         todo.editMode = true;
 
     }
